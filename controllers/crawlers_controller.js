6fdda0cebc3f867acc918f03293d3427e3a81856
Jason <vivanaranja@gmail.com>
2018-01-16 18:59:58 -0800

    initial controller

--- controllers/crawlers_controller.js
+++ controllers/crawlers_controller.js
@@ -4,15 +4,16 @@ var router = express.Router();
 //Import the model
 var burger = require('../models/crawler.js');
 
-
-
 // Create all our routes and set up logic within those routes where required.
+router.get('/', function(req, res) {
+    res.redirect('/index');
+});
 
-
-
-
-
-
+router.get('/index', function(req, res) {
+    crawler.selectAll(function(data) {
+        
+    });
+});
 
 
 // Export routes for server.js to use.
