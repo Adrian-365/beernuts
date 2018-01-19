ec29085b48c02f0523a17da41b36cbd1ffd9bfc1
Jason <vivanaranja@gmail.com>
2018-01-18 21:38:49 -0800

    the big conversion

---
+++ controllers/api_controller.js
@@ -0,0 +1,41 @@
+// dependencies
+var express = require('express');
+var router = express.Router();
+//Import the model
+var models = require("../models")
+
+// Create all our routes and set up logic within those routes where required.
+router.get('/crawlers', function (req, res) {
+    models.Crawler.findAll({})
+        .then(function (data) {
+            res.json(data);
+        })
+        .catch(function (err) {
+            console.error(err);
+        });
+});
+router.get('/crawlers/:id', function (req, res) {
+    models.Crawler.findAll({
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(function (data) {
+            res.json(data);
+        })
+        .catch(function (err) {
+            console.error(err);
+        });
+});
+router.post('/crawlers', function (req, res) {
+    models.Crawler.create(req.body)
+        .then(function (crawler) {
+            res.json(crawler)
+        })
+        .catch(function (err) {
+            console.error(err);
+        })
+});
+
+// Export routes for server.js to use.
+module.exports = router;
--- models/crawlers.js
+++ models/crawlers.js
@@ -1,16 +1,17 @@
 module.exports = function (sequelize, DataTypes) {
     var Crawler = sequelize.define("Crawler", {
         // Giving the Crawler model a name of type STRING
-        name: DataTypes.STRING
+        name: DataTypes.STRING,
+        email: DataTypes.STRING
     });
 
-    Crawler.associate = function (models) {
-        // Associating Crawler with Crawls
-        // When an Crawler is deleted, also delete any associated Crawls
-        Crawler.hasMany(models.Post, {
-            onDelete: "cascade"
-        });
-    };
+    // Crawler.associate = function (models) {
+    //     // Associating Crawler with Crawls
+    //     // When an Crawler is deleted, also delete any associated Crawls
+    //     Crawler.hasMany(models.Post, {
+    //         onDelete: "cascade"
+    //     });
+    // };
 
     return Crawler;
 };
