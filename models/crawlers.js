ed062104e10e0a7f339dd4b56b99de4bed9161d8
Jason <vivanaranja@gmail.com>
2018-01-19 15:28:56 -0800

    db seeded in sequelize

--- models/crawlers.js
+++ models/crawlers.js
@@ -1,8 +1,13 @@
 module.exports = function (sequelize, DataTypes) {
     var Crawler = sequelize.define("Crawler", {
         // Giving the Crawler model a name of type STRING
-        name: DataTypes.STRING,
-        email: DataTypes.STRING
+        username: DataTypes.STRING,
+        user_email: DataTypes.STRING,
+        user_city: DataTypes.STRING,
+        user_state: DataTypes.STRING(2),
+        user_zip: DataTypes.STRING(5),
+        user_blurb: DataTypes.STRING,
+        user_password: DataTypes.STRING
     });
 
     // Crawler.associate = function (models) {
