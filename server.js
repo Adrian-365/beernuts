21c6f5943f0642f9f0e28bf9b2b6fdcf4114fcd6
Jason <vivanaranja@gmail.com>
2018-01-20 11:55:16 -0800

    safety

--- server.js
+++ server.js
@@ -41,8 +41,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 models.sequelize.sync({ force: true }).then(function () {
     // run the sql query to seed db here
     models.Crawler.bulkCreate([{ username: "BeerMe", user_email: "vivanaranja+1beerme@gmail.com", user_city: "Irvine", user_state: "CA", user_zip: "92602", user_blurb: "Here to beer", user_password: "flimflam" }, { username: "Pilsneresque", user_email: "vivanaranja+1pilsneresque@gmail.com", user_city: "San Clemente", user_state: "CA", user_zip: "92672", user_blurb: "Tiny the Younger", user_password: "triangletime" }, { username: "Caskmaster", user_email: "vivanaranja+1caskmaster@gmail.com", user_city: "Long Beach", user_state: "CA", user_zip: "90803", user_blurb: "Nice to mead you", user_password: "showmethedubbel" }]);
-    models.Crawls.bulkCreate([]);
-    models.Places.bulkCreate([{ placeId: "ChIJ7YHUCyAw3YARfZ79M1J6u68" }, { placeId: "ChIJuYNGVh4w3YARFyshF6Esgt0" }, { placeId: "ChIJ4QrZWB4w3YARtw_bH88lsjI" }, { placeId: "ChIJI9IA4SQw3YARL-56ZCO4KiM" }]);
+    models.Crawls.bulkCreate([{ placeId: "ChIJ7YHUCyAw3YARfZ79M1J6u68" }]);
+    // models.Places.bulkCreate([{ placeId: "ChIJ7YHUCyAw3YARfZ79M1J6u68" }, { placeId: "ChIJuYNGVh4w3YARFyshF6Esgt0" }, { placeId: "ChIJ4QrZWB4w3YARtw_bH88lsjI" }, { placeId: "ChIJI9IA4SQw3YARL-56ZCO4KiM" }]);
     app.listen(port, function () {
         console.log("App listening on PORT " + port);
     });
