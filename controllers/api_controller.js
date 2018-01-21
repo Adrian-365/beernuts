da9aac349438be41692a0ddbbdab4cb7b0d274a9
Adrian Nalwalker <analwalker@planetfg.local>
2018-01-20 21:18:22 -0800

    night mode map, and turned off businesses

--- controllers/api_controller.js
+++ controllers/api_controller.js
@@ -6,38 +6,38 @@ var models = require("../models")
 
 // Create all our routes and set up logic within those routes where required.
 // Shouldn't all this be in the routes files in the routes folder? Asking for a friend.
-router.get('/crawlers', function (req, res) {
+router.get('/crawlers', function(req, res) {
     models.Crawler.findAll({})
-        .then(function (data) {
+        .then(function(data) {
             res.json(data);
         })
-        .catch(function (err) {
+        .catch(function(err) {
             console.error(err);
         });
 });
-router.get('/crawlers/:id', function (req, res) {
-    models.Crawler.findOne({ 
+router.get('/crawlers/:id', function(req, res) {
+    models.Crawler.findOne({
             where: {
                 id: req.params.id
             }
         })
-        .then(function (data) {
+        .then(function(data) {
             res.json(data);
         })
-        .catch(function (err) {
+        .catch(function(err) {
             console.error(err);
         });
 });
-router.post('/crawlers', function (req, res) {
+router.post('/crawlers', function(req, res) {
     models.Crawler.create(req.body)
-        .then(function (crawler) {
+        .then(function(crawler) {
             res.json(crawler)
         })
-        .catch(function (err) {
+        .catch(function(err) {
             console.error(err);
         })
 });
-router.delete('/crawlers/:id', function (req, res) {
+router.delete('/crawlers/:id', function(req, res) {
     models.Crawler.destroy({
             where: {
                 id: req.params.id
@@ -45,10 +45,21 @@ router.delete('/crawlers/:id', function (req, res) {
         }).then(function(crawler) {
             res.json(crawler);
         })
-        .catch(function (err) {
+        .catch(function(err) {
             console.error(err);
         })
 });
+//add a bar to a crawl
+router.post('/places/:placeId', function(req, res) {
+    models.Places.create(req.body)
+        .then(function(placeId) {
+            res.json(placeId)
+        })
+        .catch(function(err) {
+            console.error(err);
+        })
+
+})
 
 // Export routes for server.js to use.
 module.exports = router;
--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,31 +1,43 @@
+var express = require('express');
+var router = express.Router();
+
 $('.toggle').on('click', function() {
     $('.container').stop().addClass('active');
-  });
-  
-  $('.close').on('click', function() {
+});
+
+$('.close').on('click', function() {
     $('.container').stop().removeClass('active');
-  });
+});
 
-  var express = require('express');
-  var router = express.Router();
-   
-  /* GET home page. */
-  router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Node Tutorial' });
-  });
-   
-  router.post('/', function(req, res){
-  //Grab the request body
-  var body = req.body;
-   
-  var res_body = {
-  first_name: body.first_name,
-  last_name: body.last_name,
-  email: body.email
-  };
-   
-  res.render('welcome', res_body);
-  });
-   
-  module.exports = router;
+//grab the placeId from the DOM
+$('#add').on('click', function() {
+    var newPlaceID = {
+        placeID: this.value
+    };
+    console.log('place.place_id:  ' + newPlaceID)
+});
+//end grab placeId from the DOM
 
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Node Tutorial' });
+});
+
+router.post('/', function(req, res) {
+    //Grab the request body
+    var body = req.body;
+
+    var res_body = {
+        first_name: body.first_name,
+        last_name: body.last_name,
+        email: body.email
+    };
+
+    res.render('welcome', res_body);
+});
+
+
+
+
+module.exports = router;
