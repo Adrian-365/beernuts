12ed8771e772d39d9a4644c19fb545bce2986ce6
Adrian Nalwalker <analwalker@planetfg.local>
2018-01-20 22:21:41 -0800

    started the 2nd map for the specific crawls

---
+++ crawlMap.js
@@ -0,0 +1,119 @@
+// Map that searches based on the first place_id of a table.
+// Adds an icon for each place id
+
+// A get request to get all the placeId's that share a specific crawlId.
+// Place those placeId's in an array.
+// Render a google map centered on the [0] index placeId (or it's lat lng?)
+function myMap2() {
+    //map styliztion stuff:
+    var styledMapType = new google.maps.StyledMapType(
+        [
+            { elementType: 'geometry', stylers: [{ color: '#242f3e' }] },
+            { elementType: 'labels.text.stroke', stylers: [{ color: '#242f3e' }] },
+            { elementType: 'labels.text.fill', stylers: [{ color: '#746855' }] },
+            {
+                featureType: 'administrative.locality',
+                elementType: 'labels.text.fill',
+                stylers: [{ color: '#d59563' }]
+            },
+            {
+                featureType: 'poi',
+                elementType: 'labels.text.fill',
+                stylers: [{ color: '#d59563' }]
+            },
+            {
+                featureType: 'poi.park',
+                elementType: 'geometry',
+                stylers: [{ color: '#263c3f' }]
+            },
+            {
+                featureType: 'poi.park',
+                elementType: 'labels.text.fill',
+                stylers: [{ color: '#6b9a76' }]
+            },
+            {
+                featureType: 'road',
+                elementType: 'geometry',
+                stylers: [{ color: '#38414e' }]
+            },
+            {
+                featureType: 'road',
+                elementType: 'geometry.stroke',
+                stylers: [{ color: '#212a37' }]
+            },
+            {
+                featureType: 'road',
+                elementType: 'labels.text.fill',
+                stylers: [{ color: '#9ca5b3' }]
+            },
+            {
+                featureType: 'road.highway',
+                elementType: 'geometry',
+                stylers: [{ color: '#746855' }]
+            },
+            {
+                featureType: 'road.highway',
+                elementType: 'geometry.stroke',
+                stylers: [{ color: '#1f2835' }]
+            },
+            {
+                featureType: 'road.highway',
+                elementType: 'labels.text.fill',
+                stylers: [{ color: '#f3d19c' }]
+            },
+            {
+                featureType: 'transit',
+                elementType: 'geometry',
+                stylers: [{ color: '#2f3948' }]
+            },
+            {
+                featureType: 'transit.station',
+                elementType: 'labels.text.fill',
+                stylers: [{ color: '#d59563' }]
+            },
+            {
+                featureType: 'water',
+                elementType: 'geometry',
+                stylers: [{ color: '#17263c' }]
+            },
+            {
+                featureType: 'water',
+                elementType: 'labels.text.fill',
+                stylers: [{ color: '#515c6d' }]
+            },
+            {
+                featureType: 'water',
+                elementType: 'labels.text.stroke',
+                stylers: [{ color: '#17263c' }]
+            },
+            {
+                featureType: 'poi.business',
+                stylers: [{ visibility: 'off' }]
+            }
+        ], { name: 'Styled Map' });
+
+
+    //specify the map location
+    var center = new google.maps.LatLng(lat, lng);
+    //create map and add it to the page
+    var mapDiv = document.getElementById('googleMap2');
+    //set the map placement and zoom
+    map = new google.maps.Map(mapDiv, {
+        center: center,
+        zoom: 15,
+        mapTypeControloptions: {
+            mapTypeIds: ['roadmap', 'satellite', 'hybrid', 'terrain',
+                'styled_map'
+            ]
+        }
+    });
+    //Associate the styled map with the MapTypeId and set it to display.
+    map.mapTypes.set('styled_map', styledMapType);
+    map.setMapTypeId('styled_map');
+
+
+    // Add a marker at each placeId[i]. Include a number starting at 1.
+    // Below the map, render an unordered list with the name and address of each place on the map.  Include corresponding number starting at 1.
+
+};
+//END mymap2()
