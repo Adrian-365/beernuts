d7cccc84a8f16891f33acac861a370d41d346036
Sean Hagan <sphagan1088@yahoo.com>
2018-01-25 18:13:09 -0800

    mycrawls button works

--- public/assets/js/my-crawls.js
+++ public/assets/js/my-crawls.js
@@ -3,5 +3,16 @@ $(".my-crawls").on("click", function() {
     axios.get('my-crawls')
     .then(function(response){
         var crawls = response.data;
+        var ul = $('<ul class="list-group">');
+
+        console.log(crawls);
+        for( var i = 0; i < crawls.length; i++) {
+            var li = $('<li class="list-group-item">');
+            li.append(`<p>${crawls[i].createdAt}</p>`);
+            ul.append(li);
+            li.append(`<p>${crawls[i].CrawlerId}</p>`);
+            ul.append(li);
+        }
+        $('#crawl-info').append(ul);
     });
 })
